21d9c0b703315f0026f733eb3eb61902373d011f
Karan <karanchandravanshi217@gmail.com>
2024-01-15 17:58:26 +0530

    Fixed issue

--- src/components/DatePickerModal.js
+++ src/components/DatePickerModal.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Modal, View, StyleSheet } from 'react-native';
+import { Modal, View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 
 const DatePickerModal = ({ isVisible, onSelect }) => {
     const handleDateSelect = (day) => {
         const selectedDate = new Date(day.timestamp);
         onSelect(selectedDate);
+    };
+
+    const handleBackgroundPress = () => {
+        // Close the modal by passing null to onSelect
+        onSelect(null);
     };
 
     if (!isVisible) {
@@ -18,15 +23,16 @@ const DatePickerModal = ({ isVisible, onSelect }) => {
             transparent={true}
             visible={isVisible}
         >
-            <View style={styles.modalBackground}>
-                <View style={styles.modalContainer}>
-                    <Calendar
-                        onDayPress={(day) => handleDateSelect(day)}
-                        hideExtraDays={true}
-                        
-                    />
+            <TouchableWithoutFeedback onPress={handleBackgroundPress}>
+                <View style={styles.modalBackground}>
+                    <View style={styles.modalContainer}>
+                        <Calendar
+                            onDayPress={(day) => handleDateSelect(day)}
+                            hideExtraDays={true}
+                        />
+                    </View>
                 </View>
-            </View>
+            </TouchableWithoutFeedback>
         </Modal>
     );
 };
--- src/screens/OffersRWA.jsx
+++ src/screens/OffersRWA.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
 import OffersRWACard from '../components/Card/OffersRWACard';
 
 const OffersRWA = () => {
+    const navigation = useNavigation();
     return (
         <SafeAreaView style={{ backgroundColor: 'white', paddingHorizontal: 20, height: '100%' }}>
             <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', paddingVertical: 20 }}>
+                <TouchableOpacity onPress={() => navigation.goBack()}>
                 <AntDesign name="arrowleft" size={20} color="#557184" />
+                </TouchableOpacity>
                 <Text style={{ fontSize: 16, color: '#004F84', fontWeight: '800' }}>Offer RWA</Text>
                 <Text></Text>
             </View>
